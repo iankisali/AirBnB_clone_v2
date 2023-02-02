dfad1d9fc5fa99e27266fd2d456b5e31f4c0abb4
iankisali <iankisali@gmail.com>
2023-02-02 03:45:18 -0800

    script generating .tgz archive from the contents of web_static

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -5,15 +5,12 @@ from datetime import date
 
 
 def do_pack():
-    """creates a 'tgz' archive from specified folders"""
-
-    filename = strftime("%Y%m%d%H%M%S")
+    """ A script generating archive the contents of web_static folder"""
+    dates = strftime("%Y%m%d%H%M%S")
     try:
         local("mkdir -p versions")
-        local("tar -czvf versions/web_static_{}.tgz web_static/"
-              .format(filename))
-
-        return "versions/web_static_{}.tgz".format(filename)
+        local("tar -czvf versions/web_static_{}.tgz web_static/".format(dates))
+        return "versions/web_static_{}.tgz".format(dates)
 
     except Exception as e:
         return None
