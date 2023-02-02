f4b790e9cca5452c0357489ba8fe809a305d2c95
iankisali <iankisali@gmail.com>
2023-02-02 05:10:46 -0800

    script distributing archive to web servers

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,56 +1,30 @@
 #!/usr/bin/python3
-"""Compress web static package
-"""
+""" Web server distribution"""
 from fabric.api import *
-from datetime import datetime
-from os import path
+import os.path
 
-
-env.hosts = ['34.207.237.211', '54.237.3.241']
 env.user = 'ubuntu'
+env.hosts = ['54.87.234.157', '54.160.115.69']
 env.key_filename = '~/.ssh/id_rsa'
 
 
 def do_deploy(archive_path):
-        """Deploy web files to server
-        """
-        try:
-                if not (path.exists(archive_path)):
-                        return False
-
-                # upload archive
-                put(archive_path, '/tmp/')
-
-                # create target dir
-                timestamp = archive_path[-18:-4]
-                run('sudo mkdir -p /data/web_static/\
-releases/web_static_{}/'.format(timestamp))
-
-                # uncompress archive and delete .tgz
-                run('sudo tar -xzf /tmp/web_static_{}.tgz -C \
-/data/web_static/releases/web_static_{}/'
-                    .format(timestamp, timestamp))
-
-                # remove archive
-                run('sudo rm /tmp/web_static_{}.tgz'.format(timestamp))
-
-                # move contents into host web_static
-                run('sudo mv /data/web_static/releases/web_static_{}/web_static/* \
-/data/web_static/releases/web_static_{}/'.format(timestamp, timestamp))
-
-                # remove extraneous web_static dir
-                run('sudo rm -rf /data/web_static/releases/\
-web_static_{}/web_static'
-                    .format(timestamp))
-
-                # delete pre-existing sym link
-                run('sudo rm -rf /data/web_static/current')
-
-                # re-establish symbolic link
-                run('sudo ln -s /data/web_static/releases/\
-web_static_{}/ /data/web_static/current'.format(timestamp))
-        except:
-                return False
-
-        # return True on success
+    """Distributes archive to web servers"""
+    if os.path.exists(archive_path) is False:
+        return False
+    try:
+        arc = archive_path.split("/")
+        base = arc[1].strip('.tgz')
+        # upload archive
+        put(archive_path, '/tmp')
+        sudo('mkdir -p /data/web_static/releases/{}'.format(base))
+        main = "/data/web_static/releases/{}".format(base)
+        # uncompress archive and delete .tgz
+        sudo('tar -xzf /tmp/{} -C {}/'.format(arc[1], main))
+        sudo('rm /tmp/{}'.format(arc[1]))
+        sudo('mv {}/web_static/* {}/'.format(main, main))
+        sudo('rm -rf /data/web_static/current')
+        sudo('ln -s {}/ "data/web_static/current"'.format(main))
         return True
+    except:
+        return False
